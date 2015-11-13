e746626621d46d358f74293710897f268ecc8248
Nicolas Ulrich <nicolas.ulrich@auth0.com>
2015-11-13 19:14:53 -0300

    well use InProgressFragment so remove everything related to the progress bar and message

--- lock-email/src/main/java/com/auth0/lock/email/fragment/MagicLinkLoginFragment.java
+++ lock-email/src/main/java/com/auth0/lock/email/fragment/MagicLinkLoginFragment.java
@@ -34,22 +34,14 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.auth0.lock.email.R;
-import com.auth0.lock.email.event.AuthenticationStartedEvent;
-import com.auth0.lock.event.AuthenticationError;
-import com.auth0.lock.event.AuthenticationEvent;
 import com.auth0.lock.event.NavigationEvent;
 import com.auth0.lock.fragment.BaseTitledFragment;
-import com.squareup.otto.Subscribe;
 
 public class MagicLinkLoginFragment extends BaseTitledFragment {
 
     public static final String EMAIL_ARGUMENT = "EMAIL_ARGUMENT";
 
     private String email;
-
-    Button noCodeButton;
-    TextView messageTextView;
-    View progressLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,14 +50,11 @@ public class MagicLinkLoginFragment extends BaseTitledFragment {
         if (arguments != null) {
             email = arguments.getString(EMAIL_ARGUMENT);
         }
-        bus.register(this);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-
-        bus.unregister(this);
     }
 
     @Override
@@ -77,41 +66,16 @@ public class MagicLinkLoginFragment extends BaseTitledFragment {
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        noCodeButton = (Button) view.findViewById(R.id.com_auth0_email_no_magic_link_button);
+        Button noCodeButton = (Button) view.findViewById(R.id.com_auth0_email_no_magic_link_button);
         noCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 bus.post(NavigationEvent.BACK);
             }
         });
-        messageTextView = (TextView) view.findViewById(R.id.com_auth0_email_magic_link_message);
+        TextView messageTextView = (TextView) view.findViewById(R.id.com_auth0_email_magic_link_message);
         String messageFormat = getString(R.string.com_auth0_email_login_message_magic_link);
         messageTextView.setText(Html.fromHtml(String.format(messageFormat, email)));
-        TextView progressTextView = (TextView) view.findViewById(R.id.com_auth0_email_magic_link_progress_message);
-        messageFormat = getString(R.string.com_auth0_email_login_message_magic_link_in_progress);
-        progressTextView.setText(Html.fromHtml(String.format(messageFormat, email)));
-        progressLayout = view.findViewById(R.id.com_auth0_email_magic_link_progress_layout);
-    }
-
-    @SuppressWarnings("unused")
-    @Subscribe
-    public void onAuthenticationStarted(AuthenticationStartedEvent event) {
-        noCodeButton.setVisibility(View.GONE);
-        messageTextView.setVisibility(View.GONE);
-        progressLayout.setVisibility(View.VISIBLE);
-    }
-
-    @SuppressWarnings("unused")
-    @Subscribe
-    public void onAuthenticationError(AuthenticationError error) {
-        progressLayout.setVisibility(View.GONE);
-        noCodeButton.setVisibility(View.VISIBLE);
-        messageTextView.setVisibility(View.VISIBLE);
-    }
-
-    @SuppressWarnings("unused")
-    @Subscribe public void onAuthentication(AuthenticationEvent event) {
-        progressLayout.setVisibility(View.GONE);
     }
 
     @Override
