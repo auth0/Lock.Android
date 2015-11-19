765f3bdc28f9ccde8a8234fb911d7be68569dc34
Nicolas Ulrich <nikolaseu@users.noreply.github.com>
2015-11-19 00:50:18 -0300

    remove unused import

--- lock-email/src/main/java/com/auth0/lock/email/LockEmailActivity.java
+++ lock-email/src/main/java/com/auth0/lock/email/LockEmailActivity.java
@@ -29,7 +29,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
 
