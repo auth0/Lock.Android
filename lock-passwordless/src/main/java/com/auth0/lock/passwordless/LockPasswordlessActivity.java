fe477abe6fe77e0e10174fd4f8cecac64983319b
Nicolas Ulrich <nikolaseu@users.noreply.github.com>
2015-11-23 13:30:29 -0300

    deprecate lock-sms and lock-email in favor of lock-passwordless refactor LockSMSActivity and LockEmailActivity to use LockPasswordlessActivity

--- lock-passwordless/src/main/java/com/auth0/lock/passwordless/LockPasswordlessActivity.java
+++ lock-passwordless/src/main/java/com/auth0/lock/passwordless/LockPasswordlessActivity.java
@@ -77,7 +77,7 @@ public class LockPasswordlessActivity extends FragmentActivity {
 
     private static final int REQUEST_CODE = 1234;
 
-    private static final String PASSWORDLESS_TYPE_PARAMETER = "PASSWORDLESS_TYPE_PARAMETER";
+    protected static final String PASSWORDLESS_TYPE_PARAMETER = "PASSWORDLESS_TYPE_PARAMETER";
     private static final String USERNAME_PARAMETER = "USERNAME_PARAMETER";
     private static final String IS_IN_PROGRESS_PARAMETER = "IS_IN_PROGRESS_PARAMETER";
 
@@ -181,7 +181,7 @@ public class LockPasswordlessActivity extends FragmentActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        lock.getBus().register(this);
+        bus.register(this);
     }
 
     @Override
@@ -206,7 +206,7 @@ public class LockPasswordlessActivity extends FragmentActivity {
     @Override
     protected void onStop() {
         super.onStop();
-        lock.getBus().unregister(this);
+        bus.unregister(this);
     }
 
     @Override
@@ -257,7 +257,7 @@ public class LockPasswordlessActivity extends FragmentActivity {
             String country = data.getStringExtra(CountryCodeActivity.COUNTRY_CODE);
             String dialCode = data.getStringExtra(CountryCodeActivity.COUNTRY_DIAL_CODE);
             Log.d(TAG, "Picked country " + country);
-            lock.getBus().post(new CountryCodeSelectedEvent(country, dialCode));
+            bus.post(new CountryCodeSelectedEvent(country, dialCode));
         }
     }
 
