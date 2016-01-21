2604544daca9c8f8076a9640135e93f7e03beb38
Nicolas Ulrich <nikolaseu@users.noreply.github.com>
2016-01-21 15:39:20 -0300

    Add lock module

--- app/src/main/java/com/auth0/android/lock/app/DemoActivity.java
+++ app/src/main/java/com/auth0/android/lock/app/DemoActivity.java
@@ -1,4 +1,4 @@
-package com.auth0.android.lock;
+package com.auth0.android.lock.app;
 
 import android.support.v7.app.AppCompatActivity;
 
---
+++ lock/src/main/java/com/auth0/android/lock/AuthenticationCallback.java
@@ -0,0 +1,7 @@
+package com.auth0.android.lock;
+
+/**
+ * Created by nikolaseu on 1/21/16.
+ */
+public class AuthenticationCallback {
+}
