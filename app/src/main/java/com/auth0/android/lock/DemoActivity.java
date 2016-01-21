a491cc98bcb4e3e3e8096789ca2630c669d55388
lbalmaceda <balmacedaluciano@gmail.com>
2016-01-21 12:36:44 -0300

    initial commit

---
+++ app/src/main/java/com/auth0/android/lock/DemoActivity.java
@@ -0,0 +1,10 @@
+package com.auth0.android.lock;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lbalmaceda on 1/21/16.
+ */
+public class DemoActivity extends AppCompatActivity {
+
+}
