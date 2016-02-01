ecfcdb32d20397c368fdb3f80be8700a6f104f19
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-02-01 16:47:05 -0300

    handle network errors on webview flow

--- lock/src/main/java/com/auth0/android/lock/provider/WebViewActivity.java
+++ lock/src/main/java/com/auth0/android/lock/provider/WebViewActivity.java
@@ -24,15 +24,22 @@
 
 package com.auth0.android.lock.provider;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.auth0.android.lock.R;
@@ -44,6 +51,7 @@ public class WebViewActivity extends AppCompatActivity {
 
     private WebView webView;
     private ProgressBar progressBar;
+    private Button retryButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,15 +68,41 @@ public class WebViewActivity extends AppCompatActivity {
             bar.setTitle(serviceName);
         }
         webView = (WebView) findViewById(R.id.com_auth0_lock_webview);
+        webView.setVisibility(View.INVISIBLE);
         progressBar = (ProgressBar) findViewById(R.id.com_auth0_lock_progressbar);
+        progressBar.setIndeterminate(true);
+        progressBar.setMax(100);
+        retryButton = (Button) findViewById(R.id.com_auth0_lock_retry);
+        retryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                retryButton.setVisibility(View.GONE);
+                startUrlLoading();
+            }
+        });
+
+        startUrlLoading();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+    private void startUrlLoading() {
+        if (!isNetworkAvailable()) {
+            renderNetworkError();
+            return;
+        }
+
         final Intent intent = getIntent();
         final Uri uri = intent.getData();
         final String redirectUrl = uri.getQueryParameter(KEY_REDIRECT_URI);
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                if (newProgress > 0) {
+                    progressBar.setIndeterminate(false);
+                    progressBar.setProgress(newProgress);
+                }
+            }
+        });
         webView.setWebViewClient(new WebViewClient() {
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
@@ -85,18 +119,41 @@ public class WebViewActivity extends AppCompatActivity {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+                progressBar.setProgress(100);
+                progressBar.setIndeterminate(true);
                 progressBar.setVisibility(View.GONE);
+                webView.setVisibility(View.VISIBLE);
             }
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
+                progressBar.setProgress(0);
+                progressBar.setIndeterminate(false);
                 progressBar.setVisibility(View.VISIBLE);
             }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                renderNetworkError();
+            }
+
         });
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setSupportZoom(true);
         webView.getSettings().setBuiltInZoomControls(true);
         webView.loadUrl(uri.toString());
+    }
+
+    private void renderNetworkError() {
+        webView.setVisibility(View.INVISIBLE);
+        retryButton.setVisibility(View.VISIBLE);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnectedOrConnecting();
     }
 }
