f1d74cc9a8520e24719b7cecca4fecccb13dccb9
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-02-11 12:21:13 -0300

    basic layout for auto enterprise connection chooser

--- lock/src/main/java/com/auth0/android/lock/views/EnterpriseLayout.java
+++ lock/src/main/java/com/auth0/android/lock/views/EnterpriseLayout.java
@@ -25,7 +25,6 @@
 package com.auth0.android.lock.views;
 
 import android.content.Context;
-import android.support.annotation.IdRes;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 
