ec5a3d0318afee5c880b11a26154909d800396b2
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-03-22 11:05:52 -0300

    add dialogs to the native provider.

--- app/src/main/java/com/auth0/android/lock/app/CustomWebIdentityProvider.java
+++ app/src/main/java/com/auth0/android/lock/app/CustomWebIdentityProvider.java
@@ -25,19 +25,18 @@
 package com.auth0.android.lock.app;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
-import android.widget.Toast;
 
 import com.auth0.Auth0;
 import com.auth0.android.lock.provider.AuthorizeResult;
 import com.auth0.android.lock.provider.CallbackHelper;
 import com.auth0.android.lock.provider.IdentityProvider;
 import com.auth0.android.lock.provider.IdentityProviderCallback;
-import com.auth0.android.lock.provider.WebViewActivity;
 import com.auth0.authentication.result.Credentials;
 
 import java.util.HashMap;
@@ -56,7 +55,6 @@ public class CustomWebIdentityProvider implements IdentityProvider {
     private static final String KEY_ACCESS_TOKEN = "access_token";
     private static final String KEY_TOKEN_TYPE = "token_type";
     private static final String KEY_REFRESH_TOKEN = "refresh_token";
-    private static final String KEY_AUTH0_CLIENT = "auth0Client";
     private static final String KEY_RESPONSE_TYPE = "response_type";
     private static final String KEY_STATE = "state";
     private static final String KEY_CONNECTION = "connection";
@@ -69,68 +67,54 @@ public class CustomWebIdentityProvider implements IdentityProvider {
     private static final String SCOPE_TYPE_OPENID = "openid";
 
 
-    private boolean useBrowser;
     private IdentityProviderCallback callback;
     private CallbackHelper helper;
     private final Auth0 account;
     private Map<String, Object> parameters;
-    private String clientInfo;
     private String lastState;
 
     public CustomWebIdentityProvider() {
         this.helper = new CallbackHelper("com.auth0.android.lock.app");
         this.account = new Auth0(AUTH0_CLIENT_ID, AUTH0_DOMAIN);
-        this.useBrowser = false;
         this.parameters = new HashMap<>();
-    }
-
-    /**
-     * If the class authenticates with an external browser or not.
-     *
-     * @param useBrowser if the authentication is handled in a Browser.
-     */
-    public void setUseBrowser(boolean useBrowser) {
-        this.useBrowser = useBrowser;
     }
 
     public void setCallback(@Nullable IdentityProviderCallback callback) {
         this.callback = callback;
     }
 
-    public void setParameters(Map<String, Object> parameters) {
-        this.parameters = parameters != null ? new HashMap<>(parameters) : new HashMap<String, Object>();
-    }
-
     @Override
-    public void start(Activity activity, @NonNull String connectionName) {
-        if (account.getAuthorizeUrl() == null) {
-            if (callback != null) {
-                callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, com.auth0.android.lock.R.string.com_auth0_lock_social_invalid_authorize_url, null);
-            } else {
-                Log.w(TAG, "No callback set for web IdP authenticator");
-            }
-            return;
-        }
+    public void start(final Activity activity, @NonNull final String connectionName) {
+        new android.support.v7.app.AlertDialog.Builder(activity)
+                .setTitle(R.string.native_idp_title)
+                .setMessage(R.string.native_idp_message_start)
+                .setNegativeButton(R.string.native_idp_action_cancel, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        callback.onFailure(R.string.native_idp_title, R.string.native_idp_message_canceled, null);
+                    }
+                })
+                .setPositiveButton(R.string.native_idp_action_continue, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (account.getAuthorizeUrl() == null) {
+                            callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, com.auth0.android.lock.R.string.com_auth0_lock_social_invalid_authorize_url, null);
+                            return;
+                        }
 
-        //Generate random state
-        lastState = UUID.randomUUID().toString();
-
-        startAuthorization(activity, buildAuthorizeUri(account.getAuthorizeUrl(), connectionName, lastState, parameters), connectionName);
+                        //Generate random state
+                        lastState = UUID.randomUUID().toString();
+                        startAuthorization(activity, buildAuthorizeUri(account.getAuthorizeUrl(), connectionName, lastState, parameters));
+                    }
+                })
+                .setCancelable(false)
+                .show();
     }
 
-    private void startAuthorization(Activity activity, Uri authorizeUri, String connectionName) {
-        final Intent intent;
-        if (this.useBrowser) {
-            intent = new Intent(Intent.ACTION_VIEW, authorizeUri);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-            activity.startActivity(intent);
-        } else {
-            intent = new Intent(activity, WebViewActivity.class);
-            intent.setData(authorizeUri);
-            intent.putExtra(WebViewActivity.CONNECTION_NAME_EXTRA, connectionName);
-            //Improvement: let LockActivity set requestCode
-            activity.startActivityForResult(intent, WEBVIEW_AUTH_REQUEST_CODE);
-        }
+    private void startAuthorization(Activity activity, Uri authorizeUri) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, authorizeUri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        activity.startActivity(intent);
     }
 
     @Override
@@ -145,43 +129,34 @@ public class CustomWebIdentityProvider implements IdentityProvider {
 
         final Map<String, String> values = helper.getValuesFromUri(data.getIntent().getData());
         if (values.containsKey(KEY_ERROR)) {
-            Toast.makeText(activity, "Native provider failed. Access denied", Toast.LENGTH_SHORT).show();
+            new android.support.v7.app.AlertDialog.Builder(activity)
+                    .setTitle(R.string.native_idp_title)
+                    .setMessage(R.string.native_idp_message_failed)
+                    .show();
             Log.e(TAG, "Error, access denied.");
-            if (callback != null) {
-                final int message = ERROR_VALUE_ACCESS_DENIED.equalsIgnoreCase(values.get(KEY_ERROR)) ? com.auth0.android.lock.R.string.com_auth0_lock_social_access_denied_message : com.auth0.android.lock.R.string.com_auth0_lock_social_error_message;
-                callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, message, null);
-            }
+            final int message = ERROR_VALUE_ACCESS_DENIED.equalsIgnoreCase(values.get(KEY_ERROR)) ? com.auth0.android.lock.R.string.com_auth0_lock_social_access_denied_message : com.auth0.android.lock.R.string.com_auth0_lock_social_error_message;
+            callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, message, null);
         } else if (values.containsKey(KEY_STATE) && !values.get(KEY_STATE).equals(lastState)) {
-            Toast.makeText(activity, "Native provider failed. State invalid", Toast.LENGTH_SHORT).show();
+            new android.support.v7.app.AlertDialog.Builder(activity)
+                    .setTitle(R.string.native_idp_title)
+                    .setMessage(R.string.native_idp_message_failed)
+                    .show();
             Log.e(TAG, "Received state doesn't match");
             Log.d(TAG, "Expected: " + lastState + " / Received: " + values.get(KEY_STATE));
-            if (callback != null) {
-                callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, com.auth0.android.lock.R.string.com_auth0_lock_social_invalid_state, null);
-            }
+            callback.onFailure(com.auth0.android.lock.R.string.com_auth0_lock_social_error_title, com.auth0.android.lock.R.string.com_auth0_lock_social_invalid_state, null);
         } else if (values.size() > 0) {
-            Toast.makeText(activity, "Native provider succeeded.", Toast.LENGTH_SHORT).show();
+            new android.support.v7.app.AlertDialog.Builder(activity)
+                    .setTitle(R.string.native_idp_title)
+                    .setMessage(R.string.native_idp_message_succeeded)
+                    .show();
             Log.d(TAG, "Authenticated using web flow");
-            if (callback != null) {
-                callback.onSuccess(new Credentials(values.get(KEY_ID_TOKEN), values.get(KEY_ACCESS_TOKEN), values.get(KEY_TOKEN_TYPE), values.get(KEY_REFRESH_TOKEN)));
-            }
+            callback.onSuccess(new Credentials(values.get(KEY_ID_TOKEN), values.get(KEY_ACCESS_TOKEN), values.get(KEY_TOKEN_TYPE), values.get(KEY_REFRESH_TOKEN)));
         }
         return true;
     }
 
     @Override
     public void clearSession() {
-    }
-
-    public void setClientInfo(String clientInfo) {
-        this.clientInfo = clientInfo;
-    }
-
-    private Map<String, Object> buildParameters() {
-        Map<String, Object> parameters = new HashMap<>(this.parameters);
-        if (clientInfo != null) {
-            parameters.put(KEY_AUTH0_CLIENT, clientInfo);
-        }
-        return parameters;
     }
 
     private Uri buildAuthorizeUri(String url, String serviceName, String state, Map<String, Object> parameters) {
