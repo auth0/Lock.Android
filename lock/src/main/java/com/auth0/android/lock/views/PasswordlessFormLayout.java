9b8536d42a70a8caf7cbb33c9ff47dea48fb4de5
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-03-28 18:56:18 -0300

    hide some ui content when the keyboard is shown

--- lock/src/main/java/com/auth0/android/lock/views/PasswordlessFormLayout.java
+++ lock/src/main/java/com/auth0/android/lock/views/PasswordlessFormLayout.java
@@ -30,7 +30,6 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.auth0.android.lock.R;
