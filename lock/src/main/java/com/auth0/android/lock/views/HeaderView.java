3daa31802fc15a4ee7cc006a0736976450928587
Luciano Balmaceda <balmacedaluciano@gmail.com>
2016-04-29 12:04:55 -0300

    changes in the header styles

--- lock/src/main/java/com/auth0/android/lock/views/HeaderView.java
+++ lock/src/main/java/com/auth0/android/lock/views/HeaderView.java
@@ -114,9 +114,10 @@ public class HeaderView extends RelativeLayout {
     private void updateHeaderHeight() {
         text.setVisibility(currentSize == NORMAL ? VISIBLE : GONE);
         int headerHeightSmall = (int) getResources().getDimension(R.dimen.com_auth0_lock_header_height_small);
-        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, currentSize == SMALL ? headerHeightSmall : ViewGroup.LayoutParams.WRAP_CONTENT);
+        int headerHeightNormal = (int) getResources().getDimension(R.dimen.com_auth0_lock_header_height_normal);
+        LayoutParams params = new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, currentSize == SMALL ? headerHeightSmall : headerHeightNormal);
         setLayoutParams(params);
-        header.setVisibility(keyboardIsOpen ? GONE : VISIBLE);
+        setVisibility(keyboardIsOpen ? GONE : VISIBLE);
     }
 
     /**
